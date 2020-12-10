09958b302023c5c4bb076eabb4708aa23125f493
tole <tole.car@gmail.com>
2020-12-10 22:16:29 +0100

    remove ext

--- src/ext/assets/icon_remove.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function IconRemove() {
-	return <React.Fragment><span role="img" aria-label="Remove">&#x274C;</span></React.Fragment>;
-}
-
-export default IconRemove;
--- src/ext/helpers/image_paths.jsx
+++
@@ -1,3 +0,0 @@
-export const intentModelIcon = '../assets/icons/intent_model.svg';
-export const trashIcon = '../assets/icons/trash.svg';
-
