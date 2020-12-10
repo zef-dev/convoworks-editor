f506c444785a5dadea441f0219a7c98b08b65962
tole <tole.car@gmail.com>
2020-12-10 19:56:09 +0100

    commit configs

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+//const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+const path = require('path');
+module.exports = {    
+    devtool: 'source-map', // for debug purposes on production
+    mode : 'production',
+    optimization : {
+        splitChunks: {
+        cacheGroups: {
+            vendor: {
+                test: /node_modules/,
+                chunks: 'initial',
+                name: 'vendor'
+            },
+        }
+    }
+    },
+    module: {
+        rules: [
+            { 
+                test: /\.(js|jsx)$/, 
+                exclude: /node_modules/, 
+                use: {
+                    loader: 'babel-loader'}},
+                { test: /\.html$/, exclude: /node_modules/, loader: 'html-loader' },
+                { 
+                    test: /\.css$/, 
+                    loader: [ 'style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+                    loader: 'url-loader?limit=10000&minetype=application/font-woff',
+                },
+                {
+                    test: /\.(ttf|eot|svg|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                    loader: 'file-loader',
+                },
+                { 
+                    test: /\.scss$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader'],
+                },
+                { 
+                    test: /\.svg$/,
+                    use: ['svg-inline-loader'],
+                }
+        ]
+    },
+    plugins: [
+//        new HtmlWebPackPlugin({
+//            template: 'src/index.html',
+//            filename: './index.html'
+//        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        }),
+        new webpack.DefinePlugin({
+            ON_TEST: process.env.NODE_ENV === 'test'
+        })
+    ]
+};
