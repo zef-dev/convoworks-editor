82f0572e8239843d724aa4cfb4205f5098061d5f
tole <tole.car@gmail.com>
2020-12-14 22:52:27 +0100

    polifil removed

--- webpack.config.js
+++ webpack.config.js
@@ -6,70 +6,69 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const getPath = (pathToFile) => path.resolve(__dirname, pathToFile);
 const nodeExternals = require('webpack-node-externals');
 
-module.exports = {    
+module.exports = {
     devtool: 'source-map', // for debug purposes on production
-//    entry: path.resolve('src', 'index.js'),
-  externals: [nodeExternals()],
-  entry: [
-    'babel-polyfill',
-    path.resolve('src', 'index.js')
-  ],
+    //    entry: path.resolve('src', 'index.js'),
+    externals: [nodeExternals()],
+    entry: [
+        path.resolve('src', 'index.js')
+    ],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'convoworks.js',
         library: '[name]',
-//        libraryTarget: 'umd',
-        chunkFilename: '[name].js',
-          libraryExport: 'default',
-          libraryTarget: 'umd', // you can use libraries everywhere, e.g requirejs, node 
-          umdNamedDefine: true,
-      },
-    optimization : {
+        libraryExport: 'default',
+        libraryTarget: 'umd', // you can use libraries everywhere, e.g requirejs, node 
+        umdNamedDefine: true,
+    },
+    optimization: {
         splitChunks: {
-        cacheGroups: {
-            vendor: {
-                test: /node_modules/,
-                chunks: 'initial',
-                name: 'vendor'
-            },
+            cacheGroups: {
+                vendor: {
+                    test: /node_modules/,
+                    chunks: 'initial',
+                    name: 'vendor'
+                },
+            }
         }
-    }
     },
     module: {
         rules: [
-            { 
-                test: /\.(js|jsx)$/, 
-                exclude: /node_modules/, 
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader'}},
-                { test: /\.html$/, exclude: /node_modules/, loader: 'html-loader' },
-                { 
-                    test: /\.css$/, 
-                    loader: [ 'style-loader', 'css-loader'],
-                },
-                {
-                    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-                    loader: 'url-loader?limit=10000&minetype=application/font-woff',
-                },
-                {
-                    test: /\.(ttf|eot|svg|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                    loader: 'file-loader',
-                },
-                { 
-                    test: /\.scss$/,
-                    use: ['style-loader', 'css-loader', 'sass-loader'],
-                },
-                { 
-                    test: /\.svg$/,
-                    use: ['svg-inline-loader'],
+                    loader: 'babel-loader'
                 }
+            },
+            { test: /\.html$/, exclude: /node_modules/, loader: 'html-loader' },
+            {
+                test: /\.css$/,
+                loader: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&minetype=application/font-woff',
+            },
+            {
+                test: /\.(ttf|eot|svg|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader',
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.svg$/,
+                use: ['svg-inline-loader'],
+            }
         ]
     },
     plugins: [
-//        new HtmlWebPackPlugin({
-//            template: 'src/index.html',
-//            filename: './index.html'
-//        }),
+        //        new HtmlWebPackPlugin({
+        //            template: 'src/index.html',
+        //            filename: './index.html'
+        //        }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
             filename: '[name].css',
