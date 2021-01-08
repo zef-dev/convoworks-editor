c0aae6df5fbaff9e08daca9599f376d82db55458
tole <tole.car@gmail.com>
2021-01-08 12:36:30 +0100

    intents list style back v1

--- src/convoworks/editor/intents/index.js
+++ src/convoworks/editor/intents/index.js
@@ -6,6 +6,8 @@ import intentNew from './intent-new.directive';
 import intentEditor from './intent-editor.directive';
 
 import '@zef-dev/convoworks-intent-model-editor/dist/index.css'
+import './items.scss';
+
 
 /* @ngInject */
 export default angular
