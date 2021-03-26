ebb94d74027a6343c70f769cf949fb69914eadea
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2021-03-26 12:41:03 +0100

    Toggling mechanism for containers

--- src/convoworks/editor/workflow/selectable-component-buttons.directive.js
+++ src/convoworks/editor/workflow/selectable-component-buttons.directive.js
@@ -9,26 +9,36 @@ export default function selectableComponentButtons($log, $rootScope, UserPrefere
         template: template,
         link: function( $scope, $element, $attributes) {
 
-            $log.log( 'collapseExpandButtons link');
+            $log.log( 'selectableComponentButtons link');
 
             $scope.previewing   =   false;
 
             $scope.expandAll    =   function() {
-                $log.log( 'collapseExpandButtons expandAll');
+                $log.log( 'selectableComponentButtons expandAll');
                 $rootScope.$broadcast( 'ExpandAllRequested');
             }
 
             $scope.collapseAll    =   function() {
-                $log.log( 'collapseExpandButtons collapseAll');
+                $log.log( 'selectableComponentButtons collapseAll');
                 $rootScope.$broadcast( 'CollapseAllRequested');
             }
 
             $scope.toggleTitles    =   function() {
-                $log.log( 'collapseExpandButtons toggleTitles');
+                $log.log( 'selectableComponentButtons toggleTitles');
                 if ( $scope.areTitlesOn()) {
                     UserPreferencesService.registerData( 'show_default_titles', false);
                 } else {
                     UserPreferencesService.registerData( 'show_default_titles', true);
+                }
+            }
+
+            $scope.toggleContainers = function() {
+                $log.log('selectableComponentButtons toggleContainers');
+
+                if ($scope.areContainersShown()) {
+                    UserPreferencesService.registerData('show_hidden_containers', false);
+                } else {
+                    UserPreferencesService.registerData('show_hidden_containers', true);
                 }
             }
 
@@ -37,15 +47,28 @@ export default function selectableComponentButtons($log, $rootScope, UserPrefere
                 $scope.previewing = !$scope.previewing;
             }
 
-            $scope.getTitlesDescription    =   function() {
-                if ( $scope.areTitlesOn()) {
+            $scope.getTitlesDescription = function() {
+                if ($scope.areTitlesOn()) {
                     return 'Click to hide default component titles (only custom names will be displayed)';
                 }
+
                 return 'Click to display default component titles';
             }
 
-            $scope.areTitlesOn    =   function() {
-                return UserPreferencesService.get( 'show_default_titles', true);
+            $scope.getContainersDescription = function() {
+                if ($scope.areContainersShown()) {
+                    return 'Click to hide some containers that aren\'t immediately important.';
+                }
+
+                return 'Click to show all containers at all times.';
+            }
+
+            $scope.areContainersShown = function() {
+                return UserPreferencesService.get('show_hidden_containers', true);
+            }
+
+            $scope.areTitlesOn = function() {
+                return UserPreferencesService.get('show_default_titles', true);
             }
 
             $scope.getPreviewDescription    =   function() {
