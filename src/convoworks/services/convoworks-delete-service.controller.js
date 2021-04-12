881c0b16f6e21212dc9f74a99855d7230430e663
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2021-04-12 11:05:42 +0200

    Show report after service has been deleted

--- src/convoworks/services/convoworks-delete-service.controller.js
+++ src/convoworks/services/convoworks-delete-service.controller.js
@@ -5,19 +5,39 @@ export default function ConvoworksDeleteServiceController($scope, $log, $uibModa
         selected: true
     }
 
+    $scope.report = null;
+
     $scope.delete = function()
     {
         ConvoworksApi.deleteService(serviceId, $scope.localOnly.selected).then(function(report) {
-            $uibModalInstance.close(report);
-            AlertService.addSuccess('Service successfully deleted.');
+            // $uibModalInstance.close(report);
+
+            $scope.report = report;
+
+            // AlertService.addSuccess('Service successfully deleted.');
         }, function (reason) {
             $log.error(reason);
             AlertService.addDanger('Failed to delete service.');
         });
     };
 
+    $scope.shouldShow = function(section)
+    {
+        return section && !Array.isArray(section);
+    }
+
+    $scope.fixName = function(name)
+    {
+        return name.charAt(0).toUpperCase() + name.slice(1);
+    }
+
     $scope.cancel = function()
     {
         $uibModalInstance.dismiss('cancel');
+    }
+
+    $scope.ok = function()
+    {
+        $uibModalInstance.close($scope.report);
     }
 }
