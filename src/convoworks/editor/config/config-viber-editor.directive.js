a661577654ee2cf7d755556d8e670bf3e7fe9913
mmarcec007 <mark.marcec@gmail.com>
2021-04-13 10:18:28 +0200

    make sure to keep track of time_created and time_updated in config-messenger-editor.directive.js

--- src/convoworks/editor/config/config-viber-editor.directive.js
+++ src/convoworks/editor/config/config-viber-editor.directive.js
@@ -21,7 +21,9 @@ export default function configConvoChatEditor($log, $q, $rootScope, $window, Con
                 delegateNlp: null,
                 account_id: null,
                 auth_token: null,
-                event_types: []
+                event_types: [],
+                time_created: 0,
+                time_updated: 0
             };
 
             $scope.intentNlps  =   [
@@ -61,6 +63,8 @@ export default function configConvoChatEditor($log, $q, $rootScope, $window, Con
                         configBak = angular.copy( $scope.config);
                         is_new      =   false;
                         is_error    =   false;
+                        $scope.config.time_created = data.time_created;
+                        $scope.config.time_updated = data.time_created;
                         AlertService.addSuccess(`Service ${$scope.service.service_id} was linked successfully with Viber.`);
                         $rootScope.$broadcast('ServiceConfigUpdated', $scope.config);
                     }, function ( response) {
@@ -73,6 +77,8 @@ export default function configConvoChatEditor($log, $q, $rootScope, $window, Con
                         $log.debug('configConvoChatEditor update() $scope.config', $scope.config);
                         configBak = angular.copy( $scope.config);
                         is_error    =   false;
+                        $scope.config.time_created = data.time_created;
+                        $scope.config.time_updated = data.time_updated;
                         AlertService.addSuccess('Viber config updated.');
                         $rootScope.$broadcast('ServiceConfigUpdated', $scope.config);
                     }, function ( response) {
