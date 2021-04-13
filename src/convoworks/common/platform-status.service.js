18de25a281edc378823beed5a85c235089be2cc5
mmarcec007 <mark.marcec@gmail.com>
2021-04-13 13:44:38 +0200

    add new service platform-status.service.js

---
+++ src/convoworks/common/platform-status.service.js
@@ -0,0 +1,54 @@
+/* @ngInject */
+export default function PlatformStatusService( $rootScope, $log, $http, $q, $timeout, $interval, ConvoworksApi) {
+        $log.log("PlatformStatusService init");
+
+        const MAX_NUMBER_OF_TRIES = 20;
+        const TIMEOUT_LENGTH = 10000;
+
+        let timeout = [];
+        let pollTry = [];
+
+        this.checkStatus = checkStatus;
+
+        function checkStatus(serviceId, platformId)
+        {
+            $log.log('PlatformStatusService checkStatus()', serviceId, platformId);
+            ConvoworksApi.checkExternalServiceStatus(serviceId, platformId).then(function (data) {
+                $log.log('checkExternalServiceStatus', serviceId, platformId, data);
+                let broadcastPayload = {checkingServiceStatus: true, status: data.status, platformName: platformId};
+                if (data.status === 'SERVICE_PROPAGATION_STATUS_IN_PROGRESS') {
+                    _pollUntilStatusFinished(serviceId, platformId);
+                } else if (data.status !== 'SERVICE_PROPAGATION_STATUS_IN_PROGRESS') {
+                    broadcastPayload.checkingServiceStatus = false;
+                    pollTry[platformId] = 0;
+                }
+                $rootScope.$broadcast('PlatformStatusUpdated', broadcastPayload);
+            }).catch(function() {
+                let broadcastPayload = {checkingServiceStatus: false, status: '', platformName: platformId, errorMessage: 'Failed to check status of ' + platformId};
+                pollTry[platformId] = 0;
+                $rootScope.$broadcast('PlatformStatusUpdated', broadcastPayload);
+            });
+        }
+
+        function _pollUntilStatusFinished(serviceId, platformId) {
+            $log.log('_pollUntilStatusFinished()', timeout, pollTry, platformId);
+            pollTry[platformId] === undefined ? pollTry[platformId] = 0 : pollTry[platformId] = pollTry[platformId];
+
+            _cancelPoll(platformId);
+            timeout[platformId] = $timeout(function() {
+                pollTry[platformId]++;
+                if (pollTry[platformId] <= MAX_NUMBER_OF_TRIES) {
+                    checkStatus(serviceId, platformId);
+                } else {
+                    pollTry[platformId] = 0;
+                }
+            }, TIMEOUT_LENGTH);
+        }
+
+        function _cancelPoll(platformId) {
+            if (timeout[platformId] !== undefined) {
+                $timeout.cancel(timeout[platformId]);
+                timeout[platformId] = undefined;
+            }
+        }
+}
