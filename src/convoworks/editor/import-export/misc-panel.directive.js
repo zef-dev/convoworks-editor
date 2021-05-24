5b5f96ae6d0de75e6b04f0291cadde8b64ee8dc1
mmarcec007 <mark.marcec@gmail.com>
2021-05-24 11:52:48 +0200

    Disable export buttons for unpublished platforms

--- src/convoworks/editor/import-export/misc-panel.directive.js
+++ src/convoworks/editor/import-export/misc-panel.directive.js
@@ -22,6 +22,15 @@ export default function miscPanel( $log, $window, ConvoworksApi, CONVO_ADMIN_API
                 include_configurations : false
             };
 
+            $scope.serviceReleases = [];
+
+            ConvoworksApi.getServiceReleases($scope.service.service_id).then(function (data) {
+                $log.debug( 'miscPanel getServiceReleases() OK', data);
+                $scope.serviceReleases = data;
+            }, function ( reason) {
+                $log.debug( 'miscPanel getServiceReleases() reason', reason);
+            });
+
             $scope.uploadSubmitted  =   function( file)
             {
                 $log.debug( 'miscPanel uploadSubmitted() file', file, '$scope.uploadOptions', $scope.uploadOptions);
@@ -47,12 +56,23 @@ export default function miscPanel( $log, $window, ConvoworksApi, CONVO_ADMIN_API
 
             $scope.downloadPlatform  =   function( platformId)
             {
+                if (!$scope.hasPlatformRelease(platformId)) {
+                    window.alert("Can't download export for " + platformId + " at the moment. Please create an configuration for the " + platformId + ".");
+                    return;
+                }
+
                 $log.debug( 'miscPanel downloadPlatform()', platformId);
                 var url =   CONVO_ADMIN_API_BASE_URL + '/service-imp-exp/export/' + $scope.service.service_id + '/' + platformId;
                 $log.debug( 'miscPanel redirecting to ['+url+']');
                 document.location.href  =   url;
             }
 
+            $scope.hasPlatformRelease = function (platformId) {
+                return $scope.serviceReleases.filter(
+                    platformRelease => platformRelease.platform_id !== undefined
+                        && platformRelease.platform_id === platformId
+                ).length > 0;
+            }
         }
     }
 };
