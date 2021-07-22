c7244eb5b1c09bfd4f945d5d3b3853a25fee5454
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2021-07-22 11:06:28 +0200

    Include new directive

--- src/convoworks/editor/props/index.js
+++ src/convoworks/editor/props/index.js
@@ -7,10 +7,12 @@ import propertiesEditor from './properties-editor.directive';
 
 import convoIntentEditor from './convo-intent-editor.directive';
 import paramsEditor from './params-editor.directive';
+import contenteditable from './contendeditable.directive';
 
 /* @ngInject */
 export default angular
   .module('convo.editor.props', [])
+  .directive('contenteditable', contenteditable)
   .directive('propertiesEditor', propertiesEditor)
   .directive('convoIntentEditor', convoIntentEditor)
   .directive('paramsEditor', paramsEditor)
