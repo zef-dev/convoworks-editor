2b892999267b4bb361d7ba9b95ae1e2f191cffc8
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2021-07-29 09:17:01 +0200

    Remove log

--- src/convoworks/editor/props/properties-editor.directive.js
+++ src/convoworks/editor/props/properties-editor.directive.js
@@ -227,8 +227,8 @@ export default function propertiesEditor($log, $document, $transitions, $rootSco
                 ].includes(editorType);
             }
 
-            $scope.toggle = function(key) {
-                $log.log('heho', key, $scope.definition);
+            $scope.toggle = function(key)
+            {
                 const system_key = `_use_var_${key}`;
 
                 switch ($scope.component.properties[system_key]) {
