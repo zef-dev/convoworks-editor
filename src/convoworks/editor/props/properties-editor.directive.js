c33c20f0c93e56320a5faff86792d53d17d9d7ea
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2021-10-08 11:11:20 +0200

    Simplify `maybeInt`

--- src/convoworks/editor/props/properties-editor.directive.js
+++ src/convoworks/editor/props/properties-editor.directive.js
@@ -208,15 +208,17 @@ export default function propertiesEditor($log, $document, $transitions, $rootSco
                 $scope.component.properties.ok_specific[name].properties.utterances.splice( i, 1);
             };
 
-            $scope.maybeInt                     =   function( value)
-            {
-                var ret =   value * 1;
+            // $scope.maybeInt                     =   function( value)
+            // {
+            //     var ret =   value * 1;
 
-                if ( isNaN( ret))
-                    return value;
+            //     if ( isNaN( ret))
+            //         return value;
 
-                return ret;
-            };
+            //     return ret;
+            // };
+
+            $scope.maybeInt = value => isNaN(value * 1) ? value : value * 1;
 
             $scope.canToggleToRaw = function (editorType)
             {
