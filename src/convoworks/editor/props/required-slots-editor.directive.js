9b4a913eb29aa6d01d9a7e996a0074902d05bcc7
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2022-06-09 13:47:57 +0200

    Refactor toggling all required

--- src/convoworks/editor/props/required-slots-editor.directive.js
+++ src/convoworks/editor/props/required-slots-editor.directive.js
@@ -2,7 +2,7 @@ import template from './required-slots-editor.tmpl.html';
 
 /* @ngInject */
 export default function requiredSlotsEditor($log, ConvoworksApi) {
-    return  {
+    return {
         restrict: 'E',
         require: '^propertiesContext',
         template: template,
@@ -17,58 +17,32 @@ export default function requiredSlotsEditor($log, ConvoworksApi) {
 
             _init();
 
-            $scope.$watch(function() {
-                return $scope.component.properties.intent;
-            }, function (val) {
-                $log.log('requiredSlotsEditor selected intent changed', val);
+            $scope.$watch('component', (component) => {
+                $log.log('requiredSlotsEditor selected component changed', component);
                 $scope.intent_slots = {};
 
-                if (val)
-                {
-                    const intent = $scope.service.intents.filter((i) => i.name === val)[0];
-                    $scope.component.properties[$scope.key] = [];
+                if (component) {
+                    const intent = $scope.service.intents.filter((i) => i.name === component.properties.intent)[0];
 
                     if (!intent) {
                         $log.log('requiredSlotsEditor $watch undefined intent');
+                        $scope.component.properties[$scope.key] = [];
                         return;
                     }
 
                     $log.log('requiredSlotsEditor $watch got matched intent', intent);
 
-                    if (intent.utterances)
-                    {
-                        intent.utterances
-                            .map((utterance) => utterance.model)
-                            .flat()
-                            .filter((model) => model.hasOwnProperty('type'))
-                            .map((model) => {
-                                const value = model['slot_value'] || model.type.replace('@', '');
-                                const type = model.type;
-
-                                $log.log('requiredSlotsEditor utterances model map value', value, 'type', type);
-
-                                if (!$scope.intent_slots[value]) {
-                                    $scope.intent_slots[value] = {
-                                        type,
-                                        required: $scope.component.properties[$scope.key].includes(value)
-                                    };
-                                }
-
-                                $log.log('requiredSlotsEditor final intent_slots', $scope.intent_slots);
-                            });
-                    }
+                    $scope.intent_slots = _setupIntentSlots(intent);
                 }
-            });
+            }, true);
 
             $scope.$watch('intent_slots', (val) => {
                 let required_slots = [];
 
-                for (const slot in val)
-                {
+                for (const slot in val) {
                     $log.log('requiredSlotsEditor $watch intent_slots', val[slot]);
 
-                    if (val[slot].required)
-                    {
+                    if (val[slot].required) {
                         required_slots.push(slot);
                     }
                 }
@@ -76,26 +50,25 @@ export default function requiredSlotsEditor($log, ConvoworksApi) {
                 $scope.component.properties[$scope.key] = required_slots;
             }, true);
 
-            $scope.isSlotRequired = function (slotName)
-            {
+            $scope.isSlotRequired = function (slotName) {
                 return $scope.component.properties[$scope.key].includes(slotName);
             }
 
-            $scope.areAllRequired = function ()
-            {
-                let required = [];
+            $scope.areAllRequired = function () {
+                if (Object.getOwnPropertyNames($scope.intent_slots).length === 0) {
+                    return false;
+                }
 
                 for (const slot in $scope.intent_slots) {
-                    if ($scope.intent_slots[slot].required) {
-                        required.push(slot);
+                    if ($scope.intent_slots[slot].required === false) {
+                        return false;
                     }
                 }
 
-                return required.length === Object.keys($scope.intent_slots).length;
+                return true;
             }
 
-            $scope.toggleAllSlotsRequired = function ()
-            {
+            $scope.toggleAllSlotsRequired = function () {
                 const all = Object.keys($scope.intent_slots);
                 let all_required = true;
 
@@ -112,8 +85,7 @@ export default function requiredSlotsEditor($log, ConvoworksApi) {
             }
 
             // INIT
-            function _init()
-            {
+            function _init() {
                 $scope.intent_slots = {};
 
                 // non existent
@@ -121,7 +93,7 @@ export default function requiredSlotsEditor($log, ConvoworksApi) {
                     $scope.component.properties[$scope.key] = [];
                 }
 
-                 // old string separated with commas
+                // old string separated with commas
                 if (!Array.isArray($scope.component.properties[$scope.key])) {
                     if ($scope.component.properties[$scope.key].length) {
                         $scope.component.properties[$scope.key] = $scope.component.properties[$scope.key].split(',');
@@ -129,6 +101,33 @@ export default function requiredSlotsEditor($log, ConvoworksApi) {
                         $scope.component.properties[$scope.key] = [];
                     }
                 }
+            }
+
+            function _setupIntentSlots(intent)
+            {
+                if (!intent.utterances) {
+                    return {};
+                }
+
+                return intent.utterances
+                    .map((utterance) => utterance.model)
+                    .flat()
+                    .filter((model) => model.hasOwnProperty('type'))
+                    .reduce((slots, model) => {
+                        const value = model['slot_value'] || model.type.replace('@', '');
+                        const type = model.type;
+
+                        $log.log('requiredSlotsEditor utterances model map value', value, 'type', type);
+
+                        if (!slots[value]) {
+                            slots[value] = {
+                                type,
+                                required: $scope.component.properties[$scope.key].includes(value)
+                            };
+                        }
+
+                        return slots;
+                    }, {});
             }
         }
     }
